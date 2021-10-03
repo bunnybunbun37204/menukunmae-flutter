8c5b19e59aec82c3290a3ca2fcc72fc3433b41ee
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2021-10-03 14:55:44 +0700

    add string doc

--- lib/tools/utils/utils.dart
+++ lib/tools/utils/utils.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import '../classes/food.dart';
 
 class Utils {
+  /// Search the [foods] that matchs with the [ingredients]
+  ///
+  /// author `Bunyawat Naunnak`
   static List<Food> searchFoodList(List<Food> foods, List<String> ingredients) {
     List<Food> output = [];
     for (Food food in foods) {
@@ -14,11 +17,17 @@ class Utils {
     return output;
   }
 
+  /// Search the list [a] contains all list [b]
+  ///
+  /// author `Bunyawat Naunnak`
   static bool _listContainsAll<T>(List<T> a, List<T> b) {
     final setA = Set.of(a);
     return setA.containsAll(b);
   }
 
+  /// Navigate to the [screen]
+  ///
+  /// author `Bunyawat Naunnak`
   static Future<void> moveToNextScreen(
       BuildContext context, Widget screen) async {
     Navigator.push(
@@ -27,6 +36,9 @@ class Utils {
     );
   }
 
+  /// Navigate to the previous screen
+  ///
+  /// author `Bunyawat Naunnak`
   static Future<void> moveToPreviousScreen(BuildContext context) async {
     Navigator.pop(context);
   }
